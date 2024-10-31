3c831dc76882b85d0e833ec984a18aaa0757a02a
Genes1us <168219101+Genes1us@users.noreply.github.com>
2024-10-31 13:35:37 +0500

    Update main.py
    Убрал всю не нужную хрень

--- tsync/main.py
+++ tsync/main.py
@@ -16,14 +16,3 @@ bot.load_extension('cogs.settings')
 # Bot creator's -> @Genes1us, @StephanBro_YT
 
 bot.run(config.TOKEN)
-
-
-# ----
-# import requests
-# import json
-# API_URL = 'http://api.burnhouse.online:20177/api/playerdata?trg='
-# SETTINGS_FILE = 'role_settings.json'
-# ----
-# ЕЩКЕРЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕ
-#  (токен бота)
-# это как заметка просто
